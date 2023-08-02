32b667abba318b43217cdc562b0f9494f36a1c8f
Anastasiia Pavlova <Stanislava290990@gmail.com>
2023-08-02 14:36:44 +0300

    Sum added

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
-﻿Console.Write("Введите Ваше имя");
-string username = Console.ReadLine();
-Console.Write("Привет, ");
-Console.Write(username);
-.
+﻿int numberA = 31;
+int numberB = 5;
+int result = numberA + numberB;
+Console.WriteLine(result);
+
