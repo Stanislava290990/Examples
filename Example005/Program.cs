64ddf32ce693a65fc21aafabb9067d7181e2449a
Anastasiia Pavlova <Stanislava290990@gmail.com>
2023-08-02 15:29:01 +0300

    Example005 added

---
+++ Example005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "Маша")
+{
+    Console.WriteLine("Ура, это же МАША");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
